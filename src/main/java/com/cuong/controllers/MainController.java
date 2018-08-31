f939b5a90c154b3014cc37412d68ec6816a3b832
CuongGM <cuonggm48@gmail.com>
2018-09-01 04:31:39 +0700

    added card view

--- src/main/java/com/cuong/controllers/MainController.java
+++ src/main/java/com/cuong/controllers/MainController.java
@@ -1,15 +1,18 @@
 package com.cuong.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
 import com.cuong.service.WordService;
+import com.cuong.utils.PathUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.Parent;
 import javafx.scene.control.MenuItem;
-import javafx.stage.Modality;
+import javafx.scene.layout.StackPane;
 
 public class MainController extends BaseController implements Initializable {
 
@@ -21,18 +24,31 @@ public class MainController extends BaseController implements Initializable {
 	private MenuItem menuItemCard;
 
 	@FXML
-	private Label labelContent;
+	private StackPane centerPane;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+
 	}
 
 	@FXML
 	private void onActionShowCard(ActionEvent event) {
 		CardController cardController = new CardController();
 		cardController.setWordList(wordService.loadAll());
-		showModal("Card.fxml", cardController, Modality.NONE, "Card");
+		setMainPane("Card.fxml", cardController);
+
+	}
+
+	private void setMainPane(String viewFileName, Object controller) {
+		try {
+			centerPane.getChildren().clear();
+			FXMLLoader fxmlLoader = new FXMLLoader(PathUtils.getViewPath(viewFileName));
+			fxmlLoader.setController(controller);
+			Parent root = fxmlLoader.load();
+			centerPane.getChildren().add(root);
+		} catch (IOException e) {
+			LOGGER.severe(e.getMessage());
+		}
 	}
 
 }
